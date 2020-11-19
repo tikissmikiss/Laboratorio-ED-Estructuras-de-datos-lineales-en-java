64753e4843ed769b1e235cda8c53c360a04dbdea
tikissmikiss <josehercepreciado@hotmail.com>
2020-11-19 22:49:14 +0100

    Pila totalmente soportada por lista.Sin atriburtos

--- src/pila/Pila.java
+++ src/pila/Pila.java
@@ -4,8 +4,6 @@ import lista.Lista;
 
 public class Pila<Tipo> {
     private Lista<Tipo> lista;
-    private Tipo cima;
-    private int length;
 
     public Pila() {
         lista = new Lista<Tipo>();
@@ -14,26 +12,19 @@ public class Pila<Tipo> {
 
     public boolean apilar(Tipo dato) {
         lista.addDato(dato);
-        cima = dato;
-        length = lista.getLenght(); //TODO para mantener la longitud bien sincronizada
         return false;
     }
 
     public Tipo desapilar() {
-        length = lista.getLenght() - 1; //TODO actualizar con -1 porque lista lo decrementa al retornar
-        if (length > 0)
-            cima = lista.getDato(length - 1);
-        else
-            cima = null;
-        return lista.sacarDato(length);
-    }
+            return lista.sacarDato(lista.getLenght());
+        }
 
     public Tipo getCima() {
         return lista.getDato(lista.getLenght() - 1);
     }
 
     public int getLength() {
-        return length;
+        return lista.getLenght();
     }
 
     public boolean isVacia() {
@@ -50,19 +41,14 @@ public class Pila<Tipo> {
 
     public void imprimir() {
         String str = "";
-        str += " n:" + length + "\n";
-        str += " cima:" + cima + "\n";
+        str += " n:" + getLength() + "\n";
+        str += " cima:" + getCima() + "\n";
         str += " lista:\n" + lista;
         System.out.println(str);
     }
 
     @Override
     public String toString() {
-        // String str = "";
-        // str += " n:" + n + "\n";
-        // str += " cima:" + cima + "\n";
-        // str += " lista:\n" + lista;
-        // return str;
         return lista.toString();
     }
 
